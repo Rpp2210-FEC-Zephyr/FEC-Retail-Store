93f7b8a03b70cb17bbbcc356994596bd99eec905
Lovinson Dieujuste <lovinsondieujuste@Lovinsons-MBP.hsd1.fl.comcast.net>
2023-03-04 13:53:13 -0500

    Image renderer

--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -28,7 +28,10 @@ const ProductOverview = ({item}) =>{
         <div class = 'left'>
         <div class="container">
    <div class="wrapper">
-    Hello
+   <img src="https://filmswot.files.wordpress.com/2018/01/coco_dominates_chinese_box_office_.jpg" />
+      <img src="https://www.foundry.com/sites/default/files/inline-images/Images_003_0.jpg" />
+      <img src="https://www.foylefilmfestival.org/sites/default/files/COCO%20main%20image%203.jpg" />
+      <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2017%2F11%2Fcoco_c330_56b_pub-pub16n-186_rgb-2000.jpg" />
 
    </div>
 
@@ -39,7 +42,7 @@ const ProductOverview = ({item}) =>{
         World
 
         </div>
-          {item ? item.content : null}
+
         </div>
 
     )
